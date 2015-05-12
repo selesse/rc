bd1dd4ca10383178f980f9dea9fba4fb75da4de6
Alex Selesse <alex@selesse.com>
2015-05-11 23:43:40 -0400

    Print floats %.2f

--- lib/rock/climbing/calculator/cost_per_climb_calculator.rb
+++ lib/rock/climbing/calculator/cost_per_climb_calculator.rb
@@ -9,10 +9,14 @@ class CostPerClimbCalculator < PassPlanCalculator
       puts "Your plan cost $#{@pass_plan.price}"
       puts "You have climbed #{@climbing_sessions.size} different times, #{climbs_covered} of which were covered by your plan"
       climbs_not_covered = @climbing_sessions.size - climbs_covered
-      puts "With your plan, it has cost you $#{cost_per_climb.round(2)} per climb"
+      puts "With your plan, it has cost you $#{get_formatted_price(cost_per_climb)} per climb"
       if climbs_not_covered > 0
-          puts "You've paid $#{(@pass_plan.individual_rate * climbs_not_covered).round(2)} for your non-covered sessions"
-          puts "You've spent $#{((@pass_plan.individual_rate * climbs_not_covered) + @pass_plan.price).round(2)} total"
+          puts "You've paid $#{get_formatted_price(@pass_plan.individual_rate * climbs_not_covered)} for your non-covered sessions"
+          puts "You've spent $#{get_formatted_price((@pass_plan.individual_rate * climbs_not_covered) + @pass_plan.price)} total"
       end
+    end
+
+    def get_formatted_price(price)
+        format("%.2f", price.round(2))
     end
 end
